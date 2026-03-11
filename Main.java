22c9a535a37c49b68e10359ae395b725ede2c677
nameistake <dhyman@nmsu.edu>
2026-03-11 16:33:18 -0600

    3/11/2026 changes to help BitChange

--- Main.java
+++ Main.java
@@ -5,14 +5,16 @@ public class Main {
     public static void main(String[] args) {
         InputStream inputStream = new InputStream();
         Scanner S = new Scanner(System.in);
+        System.out.print("Enter the file path of the image: ");
         String filepath = S.nextLine();
+        System.out.println();
         Path AbsolutePath = Paths.get(filepath).toAbsolutePath();
         if(java.nio.file.Files.exists(AbsolutePath)){
             System.out.println("File exists at: " + AbsolutePath);
             inputStream.readFile(AbsolutePath.toString());
         } else {
             System.out.println("File does not exist at: " + AbsolutePath);
-            return;
         }
+        S.close();
     }
 }
